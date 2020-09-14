b006a14641c058431cf7a6514a20a285d7cda034
mhanauer <mhanauer@indiana.edu>
2020-09-14 18:27:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "\
+[general]\n\
+email = \"your@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
